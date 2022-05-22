a7bb9f3987987a8ea1e65c55d3377f542ffda7de
tranminhquang <quang.tm@teko.vn>
2022-05-22 11:28:22 +0700

    lesson-08

--- src/components/Usercontainer.jsx
+++ src/components/Usercontainer.jsx
@@ -1,19 +1,46 @@
-import React from "react";
-import { Grid } from "@nextui-org/react";
+import React, { useState, useEffect } from "react";
+import { Grid, Pagination } from "@nextui-org/react";
 import UserItem from "./UserItem";
-import { mockUsers } from "../constants/user";
+
+const initialPage = 1;
 
 const Usercontainer = () => {
+  const [userReponse, setUserReponse] = useState(null);
+  const [currentPage, setCurrentPage] = useState(initialPage);
+
+  const onChangePageHandler = (page) => {
+    setCurrentPage(page);
+  };
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const request = await fetch(
+        `https://reqres.in/api/users?page=${currentPage}`
+      );
+      const response = await request.json();
+      setUserReponse(response);
+    };
+
+    getUsers();
+  }, [currentPage]);
+
   return (
-    <Grid.Container gap={2}>
-      {mockUsers.map((userObject) => {
-        return (
-          <Grid xs={4} key={userObject.id}>
-            <UserItem userData={userObject} />
-          </Grid>
-        );
-      })}
-    </Grid.Container>
+    <>
+      <Grid.Container gap={2}>
+        {userReponse?.data.map((userObject) => {
+          return (
+            <Grid xs={4} key={userObject.id}>
+              <UserItem userData={userObject} />
+            </Grid>
+          );
+        })}
+      </Grid.Container>
+      <Pagination
+        total={userReponse?.total_pages}
+        initialPage={initialPage}
+        onChange={onChangePageHandler}
+      />
+    </>
   );
 };
 
